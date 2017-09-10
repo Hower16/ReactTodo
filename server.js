affa795bceb65204fb5ed9fd8f80749b99093aa4
Hower16 <miky1216@gmail.com>
2017-09-09 20:40:46 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+//Create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(request, response, next){
+  if(request.headers['x-forwarded-proto'] === 'https'){
+    response.redirect('http://' + request.hostname + request.url);
+  }
+  else{
+    next();
+  }
+});
+
+//add functionality to express app
+app.use(express.static('public'));
+
+//start server
+app.listen(PORT, function(){
+  console.log("Express server is up on port " + PORT);
+});
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  //entry is where webpack should start processing code
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  //specify the output
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      applicationStyles: 'app/styles/app.scss',
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+        {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        //regular expression
+        test: /\.jsx?$/,
+        //what folders we don't want parsed by React
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
