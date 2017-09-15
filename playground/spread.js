225751eb3f5edc3df0ae0e5660d01c8cfda29c73
Hower16 <miky1216@gmail.com>
2017-09-14 21:40:13 -0500

    Complete handleAddTodo

--- playground/spread.js
+++ playground/spread.js
@@ -1,11 +1,11 @@
-function add (a, b) {
-  return a + b;
-}
-
-console.log(add(3, 1));
-
-var toAdd = [9, 5];
-console.log(add(...toAdd));
+// function add (a, b) {
+//   return a + b;
+// }
+//
+// console.log(add(3, 1));
+// 
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
 
 var groupA = ['Jen', 'Cory'];
 var groupB = ['Vikram'];
